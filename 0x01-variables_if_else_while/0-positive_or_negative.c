06ad3fd10b55d2ee8d65b9928afb79635886d50a
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-10 04:24:56 -0800

    positve or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,9 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/* main - generates a rundom number and then determines wheather the number is positive, negative or zero */
+/* return - returns always zero */
+
 int main(void)
 {
 	int n;
@@ -11,14 +14,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n > 0)
+	if (n > 0)
 	{
-		printf("%d is postive\n",n);
+		printf("%d is positive\n", n);
 	}
-	else if(n < 0)
-		printf("%d is negative\n",n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	else
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	return (0);
 }
 
