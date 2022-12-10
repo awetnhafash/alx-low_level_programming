55758cc28be76ad35f04e6aeb9fd48e81736a48d
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-10 04:42:52 -0800

    positve or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
 /**
- * betty style doc for function main goes there
  * main - returns the sign of a rundom number gerated
- * return: returns always zero 
+ * return: always zero 
  */
-
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
