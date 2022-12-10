3ae9447f9ae253ee93a4960f259decca9bd03e54
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-10 08:14:30 -0800

    prints an alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+/**
+ * main - prints an alphabet in lowercase
+ * Return: always 0
+ */
+{
+       	int i;
+
+	for (i = 97; i < 123; i++)
+	{ putchar(i);
+	putchar('\n');
+	return (0);
+	}
+}
