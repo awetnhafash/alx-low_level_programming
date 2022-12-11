87ddc887791c70aef08259497483365a371c037f
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-11 07:08:59 -0800

    prints lowercase letters except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,19 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
- * main - prints lowercase letters except e and q 
+ * main - prints lowercase letters except e and q.
  * Return: always 0
  */
-int main(void)
+int manin(void)
 {
 	int i;
-	for(i = 97; i < 123; i++)
+
+	for (i = 97; i < 123; i++)
 
 	{
-		if (i != 101 && i!= 113)
+		if (i != 101 && i != 113)
 			putchar(i);
 	}
 	putchar('\n');
-	return (0);
+	return (0)
 }
 
