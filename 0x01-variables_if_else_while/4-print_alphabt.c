f2c0f857272a0a618bcdacc1baa9b28e23cf411f
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-11 07:12:53 -0800

    prints lower case letters except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,11 +3,11 @@
  * main - prints lowercase letters except e and q 
  * Return: always 0
  */
-int manin(void)
+int main(void)
 {
 	int i;
 
-	for(i = 97; i < 123; i++)
+	for (i = 97; i < 123; i++)
 
 	{
 		if (i != 101 && i != 113)
