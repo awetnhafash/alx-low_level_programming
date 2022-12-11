553ade9853eddb117723d8c88d2fdb129698ec1b
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-11 04:19:14 -0800

    prints lowercase letters then uppercase letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabets then upper case alphabets
+ * Return: always 0
+ */
+int mani(void)
+{
+	{int a;
+	for (a = 97; a < 123; a++)
+		putchar(a);
+	int A;
+	for (A = 65; A < 91; A++)
+		putchar(A);
+	}
+putchar('\n');
+return(0)
+}
