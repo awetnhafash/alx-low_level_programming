b53b7e479a84e0aed7053d24224318fb5fcb585d
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-12 15:31:09 -0800

    prints combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - prints combinations of three digit numbers.
+ * Return: always 0
+ */
+int main(void)
+{
+	int i, e, g;
+	
+	i= 48;
+	e=48;
+	g=48;
+
+	while (i < 58)
+	{
+		i=48;
+		while (i < 58)
+		{
+			g = 48;
+			while (g < 58)
+
+			{if (e != i && e !=g && i < g)
+				{
+					putchar (e);
+					putchar (i);
+					putchar (g);
+					if (i == 56 && e == 55 && g ==57)
+					{
+						break;
+					}putchar (',');
+					putchar (' ');
+				}
+				g++
+			}
+			i++
+		}
+		e++
+			putchar('\n');
+		return (0)
+	}
+}
