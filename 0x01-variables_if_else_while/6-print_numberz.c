552f16570f4f4e1c620538d6d9f3a59ac9f248f3
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-12 11:35:16 -0800

    prints all single base ten numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all base ten single digit numbers using putchar
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+{
+	putchar(i)
+}
+putchar ('\n');
+return (0);
+}
