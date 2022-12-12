960072c391ef31de94abb080579df10cbaa7e4a7
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-12 11:51:48 -0800

    prints all lowercase letters in revers order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase letters in revers order
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 122; i > 97; i--);
+	{
+		putchar (i);
+	}
+	putchar ('\n');
+	return (0);
+}
