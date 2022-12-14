7921a86893406da53434a035b168d69aeafb9ac8
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-14 15:16:56 -0800

    prints natural numbers below 1024 which are multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints natural numbers below 1024 that are multiple of 3 or 5.
+ * Return: always 0.
+ */
+int main(void)
+{
+	int i, j;
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			j += i
+	}
+	printf("%d\n", j);
+	return (0);
+}
