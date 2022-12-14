059f445400eba51c5a2fb8f2d67abaa8c21f1981
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-14 04:34:50 -0800

    identifies sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_sign - prints a sign of a number.
+ * @n : is an integer
+ * Return: 1 for positive 0 for zero and -1 for negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+		_putchar(43);
+	if else (n = 0)
+		return (0);
+		_putchar(48);
+	else
+		return (-1);
+		putchar(45);
+}
