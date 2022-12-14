8aafb166b951c4e26aa698f8c6deb9248c9470ec
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-14 04:37:53 -0800

    identifies sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,12 +7,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		return (1);
 		_putchar(43);
+	}
 	if else (n = 0)
+	{
 		return (0);
 		_putchar(48);
+	}
 	else
+	{
 		return (-1);
-		putchar(45);
+		_putchar(45);
+	}
 }
