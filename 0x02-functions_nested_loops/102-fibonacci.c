d755da88d400ee6025e6f6500cfaf81c0d74c7e0
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-14 15:32:17 -0800

    fibonacci seies

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints the add of the Fibonacci numbers
+ * Return: always 0.
+ * int main(void)
+ */
+{
+	int i;
+	long int n1, n2, fn;
+
+	n1 = 1;
+	n2 = 2;
+	printf("1%d, 1%1d", n1, n2);
+	for (i = 0; i < 48; i++)
+	{
+		fn = n1 + n2;
+		printf(", %1d", fn);
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("\n");
+	return (0);
+}
