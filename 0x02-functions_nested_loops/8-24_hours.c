d9629c3705b3f3cea00f08ac0dbc017390fa04fb
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-14 11:26:46 -0800

    prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,12 +18,12 @@ void jack_bauer(void)
 				{
 					if (a >= 50 && b >= 52)
 							break;
-						_putchar(a);
-						_putchar(b);
-						_putchar(58);
-						_putchar(c);
-						_putchar(d);
-						_putchar('\n');
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
 				}
 			}
 		}
