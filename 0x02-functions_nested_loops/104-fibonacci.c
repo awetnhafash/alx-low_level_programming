a617c84dbeab916ffb24fba78ec7a204b3f8ac1c
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-15 10:07:25 -0800

    prints first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,20 +5,20 @@
  */
 int main(void)
 {
-	int c, boolean, bolean2;
+	int c, boolean, boolean2;
 	long int n1, n2, fn, fn2, n11, n22;
 
 	n1 = 1;
 	n2 = 2;
 	boolean = boolean2 = 1;
-	print("%ld, %ld", n1, n2);
-	for (c = 0: c < 96; c++)
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 96; c++)
 	{
 		if (boolean)
 		{
 			fn = n1 + n2;
 			printf(", %ld", fn);
-			n1 = n2:
+			n1 = n2;
 			n2 = fn;
 		}
 		else
