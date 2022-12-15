6db8418cfdfa4590651bc9af7d7d1ec68cf5e701
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-14 22:16:39 -0800

    prints n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,12 +32,12 @@ void print_times_table(int n)
 				}
 				else
 				{
-					_putchar(((op / 100) %10) + 48);
-					_putchar(((op / 10) % 10) +48);
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
 					_putchar((op % 10) + 48);
 				}
-				_putchar('\n');
 			}
+			-putchar('\n');
 		}
 	}
 }
