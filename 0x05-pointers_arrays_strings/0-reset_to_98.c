914177e1150e0a6b9aa09bb61563a180b3000845
“awetnhafash” <awet.seyoumd2445@gmail.com>
2022-12-19 03:26:29 -0800

    resets a value of a variable into 98.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * eset_to_98 - changes the value of a variable to 98.
+ * reset_to_98 - changes the value of a variable to 98.
  * Return: no return.
  * @*n: a pointer of a variable of type integer.
+ * @n: is a pointer
  */
 void reset_to_98(int *n)
 {
